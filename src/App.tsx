f6c107db64f38d4f2aa0293267ab37de04ab9baf
tyassusanto <nasusayt@gmail.com>
2023-08-23 19:45:04 +0700

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-
+import styled from 'styled-components'
+import { ThemeContextProvider } from './components/contexts/ThemeContext'
+import TopArea from './components/TopArea';
+import { useState } from 'react';
+import { UserProps } from './types';
 
 function App() {
+
+  const [user, setUser] = useState<UserProps | null>(null)
+
+  function setUserData(user: UserProps | null): void {
+    setUser(user)
+  }
+
   return (
-    <div></div>
+    <ThemeContextProvider>
+      <Container>
+        <TopArea setUser={setUserData} />
+      </Container>
+    </ThemeContextProvider>
   );
 }
+
+const Container = styled.main`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: ${props => props.theme.colors.background};
+  padding: 3.1rem 2.4rem;
+
+  @media (min-width: 768px) {
+    padding: 3.1rem 7rem;
+  }
+`;
 
 export default App;
